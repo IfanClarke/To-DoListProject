98dff5da37d34daf778cda12417d165b5e22158e
Ifan Clarke <Ifan>
2021-03-22 10:35:44 +0000

    documentation finished

--- To-DoListProject/src/main/java/com/example/demo/controller/SubtaskController.java
+++ To-DoListProject/src/main/java/com/example/demo/controller/SubtaskController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -23,6 +24,7 @@ import com.example.demo.service.SubtaskService;
 
 @RestController
 @RequestMapping(path = "/subtask")
+@CrossOrigin
 public class SubtaskController {
 
 	
--- To-DoListProject/src/main/java/com/example/demo/controller/TaskController.java
+++ To-DoListProject/src/main/java/com/example/demo/controller/TaskController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -22,6 +23,7 @@ import com.example.demo.service.TaskService;
 
 @RestController
 @RequestMapping(path = "/task")
+@CrossOrigin
 public class TaskController {
 
 	private TaskService taskService;
